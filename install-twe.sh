8d95004a971acfb5099c56b8c452ff4a9847f0ab
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-03-28 16:46:08 -0700

    Update install-twe.sh

--- install-twe.sh
+++ install-twe.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "
+   _____      _                      ___  
+  / ____|    | |                    |__ \ 
+ | (___   ___| |_ _   _ _ __   __   __ ) |
+  \___ \ / _ \ __| | | | '_ \  \ \ / // / 
+  ____) |  __/ |_| |_| | |_) |  \ V // /_ 
+ |_____/ \___|\__|\__,_| .__/    \_/|____|
+                       | |                
+                       |_|      by foolio              
+"
 which dnf >/dev/null && { DISTRO="FEDORA"; exit 0; }
 which apt-get >/dev/null && { DISTRO="DEBIAN"; }
 
