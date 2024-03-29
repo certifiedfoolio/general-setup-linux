6aac3613a6f6b0375c2d95fd77b04f33e42bafb8
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-03-29 11:47:09 -0700

    Add files via upload

---
+++ uninst.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+which dnf >/dev/null && { DISTRO="FEDORA"; exit 0; }
+which apt-get >/dev/null && { DISTRO="DEBIAN"; }
+
+echo "Detected distro: $DISTRO"
+
+if [[ "$DISTRO" == "DEBIAN" ]]; then
+    PKGMGR="apt"
+fi
+if [[ "$DISTRO" == "FEDORA" ]]; then
+    PKGMGR="dnf"
+fi
+
+dialog --title "Uninstall Setup" \
+--backtitle "Question" \
+--yesno "Do you also want to uninstall dependencies?" 7 60
+
+response=$?
+case $response in
+   0) UNDEP="YES";;
+   1) UNDEP="NO";;
+   255) UNDEP="YES" && echo "[ESC] key pressed. 'UNDEP variable set to [YES] by default.'";;
+esac
+
+rm -rf /.git
+
+if [[ "$UNDEP" == "YES" ]]; then
+    sudo $PKGMGR remove flatpak
+    sudo $PKGMGR remove dialog
+    sudo $PKGMGR remove git
+    exit 0;
+    else
+    exit 0;
+fi
