5a61c4840ce0914a53aa2b4276252de79031df25
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-03-29 08:56:49 -0700

    Update install-twe.sh

--- install-twe.sh
+++ install-twe.sh
@@ -14,6 +14,7 @@ which dnf >/dev/null && { DISTRO="FEDORA"; exit 0; }
 which apt-get >/dev/null && { DISTRO="DEBIAN"; }
 
 if [ "$DISTRO" == "DEBIAN" ]; then sudo apt update && sudo apt install gnome-tweaks && echo "Finished." && exit 0;
-if [ "$DISTRO" == "FEDORA" ]; then sudo yum update && sudo yum install gnome-tweak-tool && echo "Finished." && exit 0;
+if [ "$DISTRO" == "FEDORA" ]; then sudo dnf update && sudo dnf install gnome-tweak-tool && echo "Finished." && exit 0;
+sleep 0.3
 
 bash /setup.sh
