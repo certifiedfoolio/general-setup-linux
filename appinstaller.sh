04e226bf4c976c57b74befcc009ad620493cb683
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-04-09 11:28:20 -0700

    Added more app install options.

--- appinstaller.sh
+++ appinstaller.sh
@@ -8,20 +8,24 @@ dialog --menu "Please choose an app to install:" 20 60 5 \
     2 "Install Vencord Patch"
     3 "Install Chromium" \
     4 "Install Spotify" \
-    5 "Install Visual Studio Code" 2>$TMPFILE
+    5 "Install Blender" \
+    6 "Install Audacity"
+    7 "Install Visual Studio Code" 2>$TMPFILE
 
 RESULT=$(cat $TMPFILE)
 
 case $RESULT in
-    1) clear && echo "Installing Discord..." && yes | flatpak install flathub com.discordapp.Discord && clear;;
-    2) clear && sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)" && clear;;
-    3) clear && echo "Installing Chromium..." && yes | flatpak install flathub org.chromium.Chromium && clear;;
-    4) clear && echo "Installing Spotify..." && yes | flatpak install flathub com.spotify.Client && clear;;
-    5) clear && echo "Installing Visual Studio Code..." && yes | flatpak install flathub com.visualstudio.code && clear;;
+    1) clear && APPECHO="Discord" && echo "Installing Discord..." && yes | flatpak install flathub com.discordapp.Discord && clear;;
+    2) clear && APPECHO="Vencord Patch" && sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)" && clear;;
+    3) clear && APPECHO="Chromium" && echo "Installing Chromium..." && yes | flatpak install flathub org.chromium.Chromium && clear;;
+    4) clear && APPECHO="Spotify" && echo "Installing Spotify..." && yes | flatpak install flathub com.spotify.Client && clear;;
+    5) clear && APPECHO="Blender" && echo "Installing Blender..." && yes | flatpak install flathub org.blender.Blender && clear;;
+    6) clear && APPECHO="Audacity" && echo "Installing Audacity..." && yes | flatpak install flathub install flathub org.audacityteam.Audacity && clear;;
+    7) clear && APPECHO="Visual Studio Code" && echo "Installing Visual Studio Code..." && yes | flatpak install flathub install flathub com.visualstudio.code && clear;;
     *) clear && bash /setup.sh;;
 esac
 
-echo "Installed."
+echo "Installed $APPECHO successfully."
 sleep 0.3
 bash /setup.sh
 
